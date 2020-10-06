2d4d41c34e6dfb7288262612d340f87e641105c7
Thomas <tom.bauer95@gmail.com>
2020-10-06 13:11:11 -0500

    got resistor_color_duo test cases passing

--- resistor-color-duo/src/resistor_color_duo.c
+++ resistor-color-duo/src/resistor_color_duo.c
@@ -1,1 +1,29 @@
 #include "resistor_color_duo.h"
+#include "stdio.h"
+#include "string.h" 
+#include "stdlib.h"
+
+
+uint16_t color_code(resistor_band_t * bands) {
+
+    //allocating for a string for later use
+    char holder_string[20]; 
+
+    //allocating for a string for later use in our return value
+    char total_resistance[20];
+
+    //finding the length of our parameter since it is resistor_band_t[] of an undefined size since its a pointer
+    int len = sizeof(bands) / sizeof(int);
+
+    //looping over our bands parameter and adding the value of enum passed in to our total_resistance
+    for(int i = 0; i < len; i-=-1) {
+        //convert int value to a str
+        sprintf(holder_string, "%d", bands[i]); 
+
+        //concat our holder_string value onto our total_resistance (ex: 1 + 0 = 10)
+        strcat(total_resistance, holder_string); 
+    }
+
+    //convert our str to a int to match defined return type
+    return atoi(total_resistance);
+}
--- resistor-color-duo/src/resistor_color_duo.h
+++ resistor-color-duo/src/resistor_color_duo.h
@@ -1,4 +1,22 @@
 #ifndef RESISTOR_COLOR_DUO_H
 #define RESISTOR_COLOR_DUO_H
+#define NELEMS(x)  (sizeof(x) / sizeof((x)[0]))
+
+typedef enum {
+    BLACK = 0,
+    BROWN = 1,
+    RED = 2,
+    ORANGE = 3,
+    YELLOW = 4,
+    GREEN = 5,
+    BLUE = 6,
+    VIOLET = 7,
+    GREY = 8,
+    WHITE = 9
+} resistor_band_t;
+
+typedef unsigned short uint16_t;
+
+uint16_t color_code(resistor_band_t[]);
 
 #endif
